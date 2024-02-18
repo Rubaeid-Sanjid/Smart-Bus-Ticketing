4cba69355fcd21e40704517c5f85c1ded8424a9d
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-02-19 04:11:19 +0600

    added event to phone number field & also added countClick condition

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 let countBookedSeats = 0;
 let totalPrice = 0;
+let countClick = 0;
 const seatBtn = document.querySelectorAll('.kbd');
 
 for(let index = 0; index< seatBtn.length; index++){
     const btn = seatBtn[index];
 
     btn.addEventListener('click', function(){
-        // changing btn color
-        const btnId = document.getElementById(btn.innerText);
-        btnId.classList.add('bg-[#1DD100]', 'text-white');
+        countClick += 1;
 
         // console.log(btnId);
 
@@ -18,37 +17,47 @@ for(let index = 0; index< seatBtn.length; index++){
         // }
         // console.log(isColored);
 
-        // adding selected seats
-        const selectedSeats = document.getElementById('selectedSeats');
-        const h3 = document.createElement('h3');
-        h3.innerText = btn.innerText + " Economy "+ " 550";
-        
-        selectedSeats.appendChild(h3);
+        if(countClick <= 4){
+            // changing btn color
+            const btnId = document.getElementById(btn.innerText);
+            btnId.classList.add('bg-[#1DD100]', 'text-white');
+            
+            // adding selected seats
+            const selectedSeats = document.getElementById('selectedSeats');
+            const h3 = document.createElement('h3');
+            h3.innerText = btn.innerText + " Economy "+ " 550";       
+            selectedSeats.appendChild(h3);
 
-        // decreamenting seats
-        const availableSeats = document.getElementById('availableSeats');
-        let countAvailableSeats = parseInt(availableSeats.innerText);
-        countAvailableSeats--;
-        availableSeats.innerText = countAvailableSeats;
+            // decreamenting seats
+            const availableSeats = document.getElementById('availableSeats');
+            let countAvailableSeats = parseInt(availableSeats.innerText);
+            countAvailableSeats--;
+            availableSeats.innerText = countAvailableSeats;
 
-        // increamenting booking seats
-        const bookedSeats = document.getElementById('bookedSeats');
-        countBookedSeats = parseInt(bookedSeats.innerText);
-        countBookedSeats++;
-        bookedSeats.innerText = countBookedSeats;
+            // increamenting booking seats
+            const bookedSeats = document.getElementById('bookedSeats');
+            countBookedSeats = parseInt(bookedSeats.innerText);
+            countBookedSeats++;
+            bookedSeats.innerText = countBookedSeats;
 
-        // calculating price
-        totalPrice = countBookedSeats * 550;
-        const totalPriceId = document.getElementById('totalPrice');
-        totalPriceId.innerText = totalPrice;
+            // calculating price
+            totalPrice = countBookedSeats * 550;
+            const totalPriceId = document.getElementById('totalPrice');
+            totalPriceId.innerText = totalPrice;
 
-        const grandTotalId = document.getElementById('grandTotal');
-        grandTotalId.innerText = totalPrice;
+            const grandTotalId = document.getElementById('grandTotal');
+            grandTotalId.innerText = totalPrice;
+        }
 
         // check phone number
-        const phoneNumber = document.getElementById('phoneNumber').value;
-        parseInt(phoneNumber);
-        console.log(typeof phoneNumber);
+        const phoneNumber = document.getElementById('phoneNumber');
+        phoneNumber.addEventListener('keyup', function(e){
+            const text = e.target.value;
+            console.log(typeof text);
+            if(text.length > 0){
+                const nextBtn = document.getElementById('nextBtn');
+                nextBtn.removeAttribute('disabled');
+            }
+        })
     })
 }
-// console.log(seatBtn.innerText);
